f1f073a375f35dd2ac1da89e3f83014bd1b539e8
Stevan Little <stevan.little@gmail.com>
2017-04-06 11:35:38 +0200

    improve docs

--- lib/UNIVERSAL/Object/Immutable.pm
+++ lib/UNIVERSAL/Object/Immutable.pm
@@ -29,30 +29,58 @@ __END__
 
 =pod
 
-=head1 IDEA
+=head1 SYNPOSIS
 
-When this is at the root of an object hierarchy, that is fine,
-but if you wish to make something immutable and inherit from
-another source, then how do you handle this?
+    # used exactly as UNIVERSAL::Object is used
+    our @ISA = ('UNIVERSAL::Object::Immutable');
 
-=head2 Multiple Inheritance
+=head1 DESCRIPTION
 
-Simply inherit from this class first so as to ensure that the
-expected version of C<BLESS> is used.
+You can use this class in the same manner that you would use
+L<UNIVERSAL::Object>, the only difference is that the instances
+created will be immutable.
+
+=head2 Why Immutability?
+
+Immutable data structures are unable to be changed after they are
+created. By placing and enforcing the guarantee of immutability,
+the users of our class no longer need to worry about a while class
+of problems that arise from mutable state.
+
+=head2 Immutability is viral
+
+Inheriting from an immutable class will make your subclass also
+immutable. This is by design.
+
+=head2 Immutability after the fact
+
+When this class is used at the root of an object hierarchy, all the
+subclasses will be immutable. However, if you wish to make an immutable
+subclass of a non-immutable class, then you have two choices.
+
+=over 4
+
+=item Multiple Inheritance
+
+Using multiple inheritance, and putting this class first in the list,
+we can be sure that the expected version of C<BLESS> is used.
 
     our @ISA = (
         'UNIVERSAL::Object::Immutable',
         'My::Super::Class'
     );
 
-=head2 Role Composition
+=item Role Composition
 
-Role compositon will result in C<BLESS> being aliased in the
-consuming package and therefore have the same effect as the
-multiple inheritance.
+Using the role compositon facilities in the L<MOP> package will result
+in C<BLESS> being aliased into the consuming package and therefore have
+the same effect as the multiple inheritance.
 
     our @ISA  = ('My::Super::Class');
     our @DOES = ('UNIVERSAL::Object::Immutable');
+    # make sure something performs the role composition ...
+
+=back
 
 =cut
 
