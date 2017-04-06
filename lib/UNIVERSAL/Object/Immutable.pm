827b8223d5839aa85ca7a27421cd2b49da8d4515
Stevan Little <stevan.little@gmail.com>
2017-04-06 02:29:46 +0200

    some notes

--- lib/UNIVERSAL/Object/Immutable.pm
+++ lib/UNIVERSAL/Object/Immutable.pm
@@ -29,6 +29,31 @@ __END__
 
 =pod
 
+=head1 IDEA
+
+When this is at the root of an object hierarchy, that is fine,
+but if you wish to make something immutable and inherit from
+another source, then how do you handle this?
+
+=head2 Multiple Inheritance
+
+Simply inherit from this class first so as to ensure that the
+expected version of C<BLESS> is used.
+
+    our @ISA = (
+        'UNIVERSAL::Object::Immutable',
+        'My::Super::Class'
+    );
+
+=head2 Role Composition
+
+Role compositon will result in C<BLESS> being aliased in the
+consuming package and therefore have the same effect as the
+multiple inheritance.
+
+    our @ISA  = ('My::Super::Class');
+    our @DOES = ('UNIVERSAL::Object::Immutable');
+
 =cut
 
 
